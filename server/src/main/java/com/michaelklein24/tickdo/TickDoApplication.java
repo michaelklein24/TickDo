9f2e371d378e65feeecfc78b0dbf556f85c88361
Michael Klein <michaelklein1997@gmail.com>
2025-07-03 22:22:56 -0500

    initial project setup

---
+++ server/src/main/java/com/michaelklein24/tickdo/TickDoApplication.java
@@ -0,0 +1,13 @@
+package com.michaelklein24.tickdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickDoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/michaelklein24/tickdo/TickDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelklein24.tickdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
